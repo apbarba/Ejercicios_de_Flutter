cb1b9dea9fa6117509028907137388e79c398874
Ana Pilar <pilarbarba03@gmail.com>
2023-01-12 22:23:58 +0100

    Implementando otra actividad siin ver que funciona

--- pokemonlist/lib/main.dart
+++ pokemonlist/lib/main.dart
@@ -96,3 +96,53 @@ Widget build(BuildContext context) {
 
 
 }
+
+/*
+class PokemonDetail extends StatelessWidget {
+  final Pokemon pokemon;
+
+  PokemonDetail({Key key, this.pokemon}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(pokemon.name),
+      ),
+      body: Column(
+        children: <Widget>[
+          Image.network(pokemon.sprites['front_default']),
+          Text("Abilities: ${pokemon.abilities.join(', ')}"),
+          Text("Pokedex Number: ${pokemon.id}"),
+        ],
+      ),
+    );
+  }
+}
+
+ListTile(
+    title: Text(pokemons[index].name),
+    onTap: () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => PokemonDetail(pokemon: pokemons[index]),
+        ),
+      );
+    },
+),
+
+Future<List<Pokemon>> getPokemons() async {
+  var response = await http.get(Uri.parse('https://pokeapi.co/api/v2/pokemon?limit=10&#'));
+  var data = json.decode(response.body);
+  var pokemons = (data['results'] as List).map((p) => Pokemon.fromJson(p)).toList();
+  for(var pokemon in pokemons){
+    var pokemonDetail = await http.get(pokemon.url);
+    var data = json.decode(pokemonDetail.body);
+    pokemon.sprites = data['sprites'];
+    pokemon.abilities = data['abilities'].map((a) => a['ability']['name']).toList();
+    pokemon.id = data['id'];
+  }
+  return pokemons;
+}
+*/
