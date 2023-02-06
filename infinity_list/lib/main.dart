ac68c9a8785486d463b5ec8d7523247a78555b3f
Ana Pilar Barba López <pilarbarba03@gmail.com>
2023-02-06 10:07:43 +0100

    Creada una lista infinita

---
+++ infinity_list/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_infinite_list/app.dart';
+import 'package:flutter_infinite_list/posts/view/posts_page.dart';
+import 'package:flutter_infinite_list/simple_bloc_observer.dart';
+
+void main() {
+  Bloc.observer = SimpleBlocObserver();
+  runApp(const App() as Widget);
+}
+
+class App extends MaterialApp {
+  const App({super.key}) : super(home: const PostsPage());
+}
