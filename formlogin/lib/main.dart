3e8676444aa0519783e4e1144729f08e082f44c8
Ana Pilar <pilarbarba03@gmail.com>
2023-02-06 22:47:24 +0100

    Validation

--- formlogin/lib/main.dart
+++ formlogin/lib/main.dart
@@ -1,116 +1,95 @@
 import 'package:flutter/material.dart';
-import 'HomePage.dart';
-import 'Registrarse.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 
-void main() {
-  runApp(MyApp());
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: LoginDemo(),
-    );
+class _LoginScreenState extends State<LoginScreen> {
+  final GlobalKey<FormBuilderState> _fbKey = GlobalKey<FormBuilderState>();
+  bool _isLogin = true;
+
+  void _toggleForm() {
+    setState(() {
+      _isLogin = !_isLogin;
+    });
   }
-}
 
-class LoginDemo extends StatefulWidget {
-  @override
-  _LoginDemoState createState() => _LoginDemoState();
-}
-
-class _LoginDemoState extends State<LoginDemo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text("Login Page"),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(top: 60.0),
-              child: Center(
-                child: Container(
-                  width: 200,
-                  height: 150,
-                  child: Image(
-                    image: NetworkImage(
-                        'https://images.vexels.com/media/users/3/129607/isolated/preview/f526eafda1a791f4a93b1ad7c62161e5-logotipo-de-hamburguesas-1.png'),
-                  ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: FormBuilder(
+          key: _fbKey,
+          child: Column(
+            children: <Widget>[
+              if (_isLogin)
+                FormBuilderTextField(
+                  attribute: "email",
+                  decoration: InputDecoration(labelText: "Email"),
+                  validators: [
+                    FormBuilderValidators.email(),
+                    FormBuilderValidators.required(),
+                  ],
                 ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 15),
-              child: TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Email',
-                    hintText: 'Enter valid email'),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                  left: 15.0, right: 15.0, top: 15, bottom: 0),
-              child: TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Password',
-                    hintText: 'Enter secure password'),
-              ),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: Text(
-                '¿Olvidastes la contraseña?',
-                style: TextStyle(
-                    color: Color.fromARGB(255, 0, 0, 0), fontSize: 15),
-              ),
-            ),
-            Container(
-              height: 50,
-              width: 250,
-              decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 0, 54, 153),
-                  borderRadius: BorderRadius.circular(10)),
-              child: TextButton(
+              if (_isLogin)
+                FormBuilderTextField(
+                  attribute: "password",
+                  decoration: InputDecoration(labelText: "Password"),
+                  validators: [
+                    FormBuilderValidators.required(),
+                    FormBuilderValidators.minLength(8),
+                  ],
+                  obscureText: true,
+                ),
+              if (!_isLogin)
+                FormBuilderTextField(
+                  attribute: "name",
+                  decoration: InputDecoration(labelText: "Name"),
+                  validators: [
+                    FormBuilderValidators.required(),
+                  ],
+                ),
+              if (!_isLogin)
+                FormBuilderTextField(
+                  attribute: "email",
+                  decoration: InputDecoration(labelText: "Email"),
+                  validators: [
+                    FormBuilderValidators.email(),
+                    FormBuilderValidators.required(),
+                  ],
+                ),
+              if (!_isLogin)
+                FormBuilderTextField(
+                  attribute: "password",
+                  decoration: InputDecoration(labelText: "Password"),
+                  validators: [
+                    FormBuilderValidators.required(),
+                    FormBuilderValidators.minLength(8),
+                  ],
+                  obscureText: true,
+                ),
+              SizedBox(height: 20),
+              RaisedButton(
+                child: Text(_isLogin ? "Login" : "Register"),
                 onPressed: () {
-                  Navigator.push(
-                      context, MaterialPageRoute(builder: (_) => HomePage()));
+                  if (_fbKey.currentState.saveAndValidate()) {
+                    print(_fbKey.currentState.value);
+                    // Aquí puedes enviar los datos del formulario a una API o base de datos para procesarlos.
+                  }
                 },
-                child: Text(
-                  'Iniciar Sesión',
-                  style: TextStyle(
-                      color: Color.fromARGB(255, 255, 255, 255), fontSize: 25),
-                ),
               ),
-            ),
-            Container(
-              height: 50,
-              width: 250,
-              decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 0, 54, 153),
-                  borderRadius: BorderRadius.circular(10)),
-              child: TextButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (_) => Registrarse()));
-                },
-                child: Text(
-                  'Registrarse',
-                  style: TextStyle(color: Colors.white, fontSize: 25),
-                ),
+              FlatButton(
+                child: Text(_isLogin ? "Register" : "Login"),
+                onPressed: _toggleForm,
               ),
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
